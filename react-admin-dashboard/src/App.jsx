23950b1bbd6dcb2df40822e5c7a3b587e465b6dd
aya <ayasabry551999@gmail.com>
2025-10-03 17:56:11 -0700

    taillwind sat

---
+++ react-admin-dashboard/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">React + Tailwind!</h1>
+      <p className="mt-4 text-gray-700">مشروع جاهز للعمل مع Tailwind CSS.</p>
+
+      <div className="bg-red-500 text-white p-4 mt-4 font-bold">
+        Tailwind يعمل تمام!
+      </div>
+    </div>
+  );
+}
+
+export default App;
